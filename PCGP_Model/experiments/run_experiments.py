IT WORKS, data was giving wrong shape

PCGP_Model/experiments/run_experiments.py
@@ -15,12 +15,11 @@ outputdir = r'experiments/output/'
 pathlib.Path(outputdir).mkdir(exist_ok=True)
 
 output_dims = [1]
-ns = [500, 1000, 1500]
-# test_functions = ['borehole', 'otlcircuit', 'piston']
-test_functions = ['borehole']
+ns = [25, 50, 100, 250, 500]
+test_functions = ['borehole', 'otlcircuit', 'piston']
 ntest = 150
 noise_level = 0.05
-n_reps = 1
+n_reps = 3
 
 def calculate_rmse(ytrue, ypred):
     rmse = np.sqrt(np.mean((ytrue - ypred) ** 2))

PCGP_Model/experiments/testfunctions/TestingfunctionBorehole.py
@@ -10,52 +10,59 @@ def query_func_meta():
     }
 
 def tstd2theta(tstd, hard=True):
-    if tstd.ndim < 1.5:
-        tstd = tstd[:, None].T
-    (Treffs, Hus, LdKw, powparams) = np.split(tstd, tstd.shape[1], axis=1)
+    if tstd.ndim == 1:
+        tstd = tstd.reshape(1, -1)
+    
+    Treffs, Hus, LdKw, powparams = tstd[:, 0], tstd[:, 1], tstd[:, 2], tstd[:, 3]
 
-    Treff = (0.5 - 0.05) * Treffs + 0.05
-    Hu = Hus * (1110 - 990) + 990
+    Treff = (0.5 - 0.05) * Treffs + 0.05  
+    Hu = Hus * (1110 - 990) + 990      
+    
     if hard:
         Ld_Kw = LdKw * (1680 / 1500 - 1120 / 15000) + 1120 / 15000
     else:
         Ld_Kw = LdKw * (1680 / 9855 - 1120 / 12045) + 1120 / 12045
 
-    powparam = powparams * (0.5 - (-0.5)) + (-0.5)
+    powparam = powparams * (0.5 - (-0.5)) + (-0.5)  
 
-    return np.hstack((Hu, Ld_Kw, Treff, powparam))
+    return np.column_stack((Hu, Ld_Kw, Treff, powparam))
 
 def xstd2x(xstd):
-    if xstd.ndim < 1.5:
-        xstd = xstd[:, None].T
-    (rws, Hls) = np.split(xstd, xstd.shape[1], axis=1)
+    if xstd.ndim == 1:
+        xstd = xstd.reshape(1, -1)
+    
+    rws, Hls = xstd[:, 0], xstd[:, 1]
 
     rw = rws * (np.log(0.5) - np.log(0.05)) + np.log(0.05)
-    rw = np.exp(rw)
-    Hl = Hls * (820 - 700) + 700
+    rw = np.exp(rw) 
+    Hl = Hls * (820 - 700) + 700 
 
-    return np.hstack((rw, Hl))
-
-def borehole_vec(x, theta):
-    (Hu, Ld_Kw, Treff, powparam) = np.split(theta, theta.shape[1], axis=1)
-    (rw, Hl) = np.split(x, x.shape[1], axis=1)
-    numer = 2 * np.pi * (Hu - Hl)
-    denom1 = 2 * Ld_Kw / rw ** 2
-    denom2 = Treff
-    return ((numer / ((denom1 + denom2))) * np.exp(powparam * rw)).reshape(-1)
+    return np.column_stack((rw, Hl))
 
 def borehole_model(x, theta):
-    theta = tstd2theta(theta)
-    x = xstd2x(x)
-    p = x.shape[0]
-    n = theta.shape[0]
-
-    theta_stacked = np.repeat(theta, repeats=p, axis=0)
-    x_stacked = np.tile(x.astype(float), (n, 1))
-
-    f = borehole_vec(x_stacked, theta_stacked).reshape((n, p))
-    return f.T
+    theta_actual = tstd2theta(theta)
+    x_actual = xstd2x(x)
+    
+    n = x_actual.shape[0]
+    
+    rw = x_actual[:, 0]    
+    Hl = x_actual[:, 1]   
+    Hu = theta_actual[:, 0]     
+    Ld_Kw = theta_actual[:, 1]   
+    Treff = theta_actual[:, 2]  
+    powparam = theta_actual[:, 3] 
+    
+    numer = 2 * np.pi * (Hu - Hl)
+    denom1 = 2 * Ld_Kw / (rw ** 2)
+    denom2 = Treff
+    
+    flow_rate = (numer / (denom1 + denom2)) * np.exp(powparam * rw)
+    
+    return flow_rate.reshape(-1, 1)
 
 def borehole_true(x):
-    theta0 = np.atleast_2d(np.array([0.5] * 4))
-    return borehole_model(x, theta0)
+    """True borehole function with theta = [0.5, 0.5, 0.5, 0.5]"""
+    n = x.shape[0]
+    theta0 = np.tile([0.5, 0.5, 0.5, 0.5], (n, 1))  
+    result = borehole_model(x, theta0)
+    return result

PCGP_Model/experiments/testfunctions/TestingfunctionOTLcircuit.py
@@ -3,40 +3,42 @@ import numpy as np
 def query_func_meta():
     return {
         'function': 'OTLcircuit',
-        'xdim': 4,  # Rb1, Rb2, Rc1, Rc2
-        'thetadim': 2,  # Rf, beta
+        'xdim': 4, 
+        'thetadim': 2,
         'c_array': np.arange(0, 10, 0.01),
-        'fail_array': np.array([])  # No failure cases by default
+        'fail_array': np.array([]) 
     }
 
-def tstd2theta(tstd):
-    """Convert standardized theta in [0,1] to physical parameters"""
-    if tstd.ndim < 1.5:
-        tstd = tstd[:, None].T
-    (Rfs, betas) = np.split(tstd, tstd.shape[1], axis=1)
+def tstd2theta_otl(tstd):
+    if tstd.ndim == 1:
+        tstd = tstd.reshape(1, -1)
     
-    Rf = 0.5 + Rfs * (3 - 0.5)  # Rf ∈ [0.5, 3]
-    beta = 50 + betas * (300 - 50)  # beta ∈ [50, 300]
+    Rfs, betas = tstd[:, 0], tstd[:, 1]
     
-    return np.hstack((Rf, beta))
+    Rf = 0.5 + Rfs * (3 - 0.5) 
+    beta = 50 + betas * (300 - 50) 
+    
+    return np.column_stack((Rf, beta))
 
-def xstd2x(xstd):
-    """Convert standardized x in [0,1] to physical parameters"""
-    if xstd.ndim < 1.5:
-        xstd = xstd[:, None].T
-    (Rb1s, Rb2s, Rc1s, Rc2s) = np.split(xstd, xstd.shape[1], axis=1)
+def xstd2x_otl(xstd):
+    if xstd.ndim == 1:
+        xstd = xstd.reshape(1, -1)
     
-    Rb1 = 50 + Rb1s * (150 - 50)  # Rb1 ∈ [50, 150]
-    Rb2 = 25 + Rb2s * (75 - 25)   # Rb2 ∈ [25, 75]
-    Rc1 = 1.2 + Rc1s * (2.5 - 1.2)  # Rc1 ∈ [1.2, 2.5]
-    Rc2 = 0.25 + Rc2s * (1.2 - 0.25)  # Rc2 ∈ [0.25, 1.2]
+    Rb1s, Rb2s, Rc1s, Rc2s = xstd[:, 0], xstd[:, 1], xstd[:, 2], xstd[:, 3]
     
-    return np.hstack((Rb1, Rb2, Rc1, Rc2))
+    Rb1 = 50 + Rb1s * (150 - 50)  
+    Rb2 = 25 + Rb2s * (75 - 25)   
+    Rc1 = 1.2 + Rc1s * (2.5 - 1.2)  
+    Rc2 = 0.25 + Rc2s * (1.2 - 0.25)  
+    
+    return np.column_stack((Rb1, Rb2, Rc1, Rc2))
 
-def OTLcircuit_vec(x, theta):
-    """Vectorized computation of OTL circuit voltage"""
-    (Rb1, Rb2, Rc1, Rc2) = np.split(x, x.shape[1], axis=1)
-    (Rf, beta) = np.split(theta, theta.shape[1], axis=1)
+def OTLcircuit_model(x, theta):
+    theta_actual = tstd2theta_otl(theta)
+    x_actual = xstd2x_otl(x)
+    
+    Rb1, Rb2, Rc1, Rc2 = x_actual[:, 0], x_actual[:, 1], x_actual[:, 2], x_actual[:, 3]
+    Rf, beta = theta_actual[:, 0], theta_actual[:, 1]
     
     Vb1 = 12 * Rb2 / (Rb1 + Rb2)
     term1 = (Vb1 + 0.74) * beta * (Rc2 + 9)
@@ -45,22 +47,11 @@ def OTLcircuit_vec(x, theta):
     term4 = beta * (Rc2 + 9) + Rf
     
     Vm = (term1 / term2) + (term3 / term4)
-    return Vm.reshape(-1)
-
-def OTLcircuit_model(x, theta):
-    """Main model function matching LCGP interface"""
-    theta = tstd2theta(theta)
-    x = xstd2x(x)
-    p = x.shape[0]
-    n = theta.shape[0]
-
-    theta_stacked = np.repeat(theta, repeats=p, axis=0)
-    x_stacked = np.tile(x.astype(float), (n, 1))
-
-    f = OTLcircuit_vec(x_stacked, theta_stacked).reshape((n, p))
-    return f.T
+    
+    return Vm.reshape(-1, 1)
 
 def OTLcircuit_true(x):
-    """True function at default theta = [0.5, 0.5]"""
-    theta0 = np.atleast_2d(np.array([0.5] * 2))
-    return OTLcircuit_model(x, theta0)
+    n = x.shape[0]
+    theta0 = np.tile([0.5, 0.5], (n, 1))  
+    result = OTLcircuit_model(x, theta0)
+    return result

PCGP_Model/experiments/testfunctions/TestingfunctionPiston.py
@@ -8,17 +8,16 @@ def query_func_meta():
     }
 
 def Piston_model(x, theta=None):
-    """Piston simulation function"""
-    if theta is not None:
-        x = np.hstack((x, theta))
+    if x.ndim == 1:
+        x = x.reshape(1, -1)
     
-    M = 30 * x[:, 0] + 30
+    M = 30 * x[:, 0] + 30      
     S = 0.015 * x[:, 1] + 0.005
     V0 = 0.008 * x[:, 2] + 0.002
-    k = 3000 * x[:, 3] + 1000
+    k = 3000 * x[:, 3] + 1000   
     P0 = 90000 * x[:, 4] + 10000
-    Ta = 290 * x[:, 5] + 10
-    T0 = 340 * x[:, 6] + 10
+    Ta = 290 * x[:, 5] + 10     
+    T0 = 340 * x[:, 6] + 10     
     
     A = P0 * S + 19.62 * M - (k * V0 / S)
     V = (S / (2 * k)) * (np.sqrt(A**2 + 4 * k * P0 * V0 / T0 * Ta) - A)

PCGP_Model/experiments/testfunctions/TestingfunctionWingweight.py
@@ -8,20 +8,19 @@ def query_func_meta():
     }
 
 def Wingweight_model(x, theta=None):
-    """Wing weight function"""
-    if theta is not None:
-        x = np.hstack((x, theta))
+    if x.ndim == 1:
+        x = x.reshape(1, -1)
     
-    Sw = 150 * x[:, 0] + 50
-    Wfw = 20 * x[:, 1] + 10
-    A = 6 * x[:, 2] + 4
-    Lambda = np.deg2rad(10 * x[:, 3] - 10)
-    q = 16 * x[:, 4] - 8
-    l = 0.5 * x[:, 5] + 0.5
-    tc = 0.1 * x[:, 6] + 0.08
-    Nz = 2.5 * x[:, 7] + 1.5
-    Wdg = 2000 * x[:, 8] + 1000
-    Wp = 1000 * x[:, 9] + 200
+    Sw = 150 * x[:, 0] + 50     
+    Wfw = 20 * x[:, 1] + 10     
+    A = 6 * x[:, 2] + 4       
+    Lambda = np.deg2rad(10 * x[:, 3] - 10)  
+    q = 16 * x[:, 4] - 8        
+    l = 0.5 * x[:, 5] + 0.5     
+    tc = 0.1 * x[:, 6] + 0.08    
+    Nz = 2.5 * x[:, 7] + 1.5     
+    Wdg = 2000 * x[:, 8] + 1000  
+    Wp = 1000 * x[:, 9] + 200    
     
     term1 = 0.036 * Sw**0.758 * Wfw**0.0035
     term2 = (A / np.cos(Lambda)**2)**0.6